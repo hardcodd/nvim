ba53a4c91eed6473d29512bca4504cb4e8d476b0
hardcodd <shubenkin.alexander@gmail.com>
2022-10-28 07:41:28 +0700

    lspconfig fix deprecations

--- lua/plugins/lspconfig/init.lua
+++ lua/plugins/lspconfig/init.lua
@@ -32,7 +32,7 @@ local handlers = {
 	["textDocument/signatureHelp"] = vim.lsp.with(vim.lsp.handlers.signature_help, diagnostics.float),
 }
 
-local capabilities = require("cmp_nvim_lsp").update_capabilities(vim.lsp.protocol.make_client_capabilities())
+local capabilities = require("cmp_nvim_lsp").default_capabilities(vim.lsp.protocol.make_client_capabilities())
 capabilities.textDocument.completion.completionItem.snippetSupport = true
 
 local on_attach = function(client, bufnr)
