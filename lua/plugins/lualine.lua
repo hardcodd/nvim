2565d9fa99b97e09c7b10205ba39e51993b6a6cb
Alex Hardcodd <shubenkin.alexander@gmail.com>
2024-01-19 05:06:26 +0700

    lualine

---
+++ lua/plugins/lualine.lua
@@ -0,0 +1,10 @@
+return {
+	"nvim-lualine/lualine.nvim",
+	dependencies = { "nvim-tree/nvim-web-devicons" },
+	opts = {
+		options = {
+			section_separators = { left = "", right = "" },
+			component_separators = { left = "", right = "" },
+		},
+	},
+}
