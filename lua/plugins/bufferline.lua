4ca4bff17304ee72e657b1389d585cc21e34f408
Alex <shubenkin.alexander@gmail.com>
2025-04-29 07:21:09 +0700

    telescope exclude

--- lua/plugins/bufferline.lua
+++ lua/plugins/bufferline.lua
@@ -1,19 +1,19 @@
 return {
-	"akinsho/bufferline.nvim",
-	version = "*",
-	dependencies = { "nvim-tree/nvim-web-devicons", "catppuccin" },
-	config = function()
-		require("bufferline").setup({
-			options = {
-				separator_style = "slope",
-				diagnostics = "nvim_lsp",
-				---@diagnostic disable-next-line: unused-local
-				diagnostics_indicator = function(count, level, diagnostics_dict, context)
-					local icon = level:match("error") and " " or " "
-					return " " .. icon .. count
-				end,
-			},
-			highlights = require("catppuccin.groups.integrations.bufferline").get(),
-		})
-	end,
+  "akinsho/bufferline.nvim",
+  version = "*",
+  dependencies = { "nvim-tree/nvim-web-devicons", "catppuccin" },
+  config = function()
+    require("bufferline").setup({
+      options = {
+        separator_style = "thin",
+        diagnostics = "nvim_lsp",
+        ---@diagnostic disable-next-line: unused-local
+        diagnostics_indicator = function(count, level, diagnostics_dict, context)
+          local icon = level:match("error") and " " or " "
+          return " " .. icon .. count
+        end,
+      },
+      highlights = require("catppuccin.groups.integrations.bufferline").get(),
+    })
+  end,
 }
--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -3,6 +3,7 @@ return {
   name = "catppuccin",
   priority = 1000,
   config = function(_, opts)
+    opts.transparent_background = true
     require("catppuccin").setup(opts)
     vim.cmd.colorscheme("catppuccin")
     vim.opt.background = "light"
