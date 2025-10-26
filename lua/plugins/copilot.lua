7d55d23de3bfed7fdb9411cbc9743ac8df644a65
Alex <shubenkin.alexander@gmail.com>
2025-10-26 18:19:55 +0700

    copilot node update

--- lua/plugins/copilot.lua
+++ lua/plugins/copilot.lua
@@ -1,7 +1,7 @@
 return {
 	"github/copilot.vim",
-  event = "InsertEnter",
+	event = "InsertEnter",
 	config = function()
-		vim.g.copilot_node_command = "~/.nvm/versions/node/v20.17.0/bin/node"
+		vim.g.copilot_node_command = "~/.nvm/versions/node/v25.0.0/bin/node"
 	end,
 }
